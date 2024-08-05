db20689895f89dc527a184ebb86e4102d70efaaf
EUSUNGKIM <jimmy0818@naver.com>
2024-08-05 22:26:21 +0900

    division remainder
    나머지 구하기

---
+++ Problem003/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem003
+{
+    /// <summary>
+    /// 2024년 8월 5일
+    /// 문제 : 나머지 구하기
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/120810
+    /// </summary>
+    internal class Program
+    {
+        public class Solution
+        {
+            public int solution(int num1, int num2)
+            {
+                int answer = num1 % num2;
+                return answer;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            int num1 = 3;
+            int num2 = 6;
+
+            Console.WriteLine(s.solution(num1, num2));
+        }
+    }
+}
