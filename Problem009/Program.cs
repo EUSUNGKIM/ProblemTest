46e90dd2d3808a73b35d867fd054f69b98ce5c02
EUSUNGKIM <jimmy0818@naver.com>
2024-08-06 23:11:02 +0900

    protractor
    각도기

---
+++ Problem009/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem009
+{
+    /// <summary>
+    /// 2024년 8월 6일
+    /// 문제 : 각도기
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/120829
+    /// </summary>
+    internal class Program
+    {
+        public class Solution
+        {
+            public int solution(int angle)
+            {
+                int answer = angle < 90 ? 1 : angle == 90 ? 2 : angle < 180 ? 3 : 4;
+                return answer;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            int num1 = 91;
+
+            Console.WriteLine(s.solution(num1));
+        }
+    }
+}
